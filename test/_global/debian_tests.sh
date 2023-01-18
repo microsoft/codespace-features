4208ed2291eaf2a2eec984b3eec1c09005334698
Mark Phippard <markphip@gmail.com>
2023-01-18 15:29:37 +0000

    Reorder checks

--- test/_global/debian_tests.sh
+++ test/_global/debian_tests.sh
@@ -7,8 +7,9 @@ source dev-container-features-test-lib
 
 check "git-version" bash -c "git --version | grep 'vfs'"
 check "gcm-version" grep "2.0\." <(git-credential-manager --version)
-check "git-config" grep "/usr/local/bin/git-credential-manager" <(cat /tmp/debian_tests/.git/config)
 check "no-dirs"  grep -v "drwxr" <(ls -l /tmp/debian_tests)
+check "git-config" grep "/usr/local/bin/git-credential-manager" <(cat /tmp/debian_tests/.git/config)
+
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
