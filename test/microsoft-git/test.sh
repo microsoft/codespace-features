3a369e202ab288b118cb1f574760ed0a94c74b71
Mark Phippard <markphip@gmail.com>
2023-01-23 14:55:25 +0000

    Improve tests by running a clone

--- test/microsoft-git/test.sh
+++ test/microsoft-git/test.sh
@@ -5,6 +5,7 @@ set -e
 source dev-container-features-test-lib
 
 check "version" bash -c "git --version | grep 'vfs'"
+check "clone" scalar clone --single-branch https://github.com/devcontainers/features /tmp/scalar-clone
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
