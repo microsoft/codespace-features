61cc5857cca1c7e820ebbe6b4c2414a74f551b53
Mark Phippard <markphip@gmail.com>
2023-02-23 12:28:37 -0500

    Add support for npm and yarn (#25)

--- test/artifacts-helper/test.sh
+++ test/artifacts-helper/test.sh
@@ -8,6 +8,8 @@ source dev-container-features-test-lib
 # Feature-specific tests
 check "dotnet" grep "pkgs.dev.azure.com" <(cat /usr/local/bin/run-dotnet.sh)
 check "nuget" grep "pkgs.dev.azure.com" <(cat /usr/local/bin/run-nuget.sh)
+check "write-npm" /usr/local/bin/write-npm.sh pkgs.dev.azure.com && grep "pkgs.dev.azure.com" <(cat ~/.npmrc)
+
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
