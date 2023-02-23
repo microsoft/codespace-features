aaf18cf58a2f0c901f35eaf141b495441fec5efa
Mark Phippard <markphip@gmail.com>
2023-02-23 01:38:49 +0000

    Fix problems from tests

--- src/artifacts-helper/install.sh
+++ src/artifacts-helper/install.sh
@@ -15,17 +15,29 @@ fi
 # Change to the directory where this script is located
 cd "$(dirname "$0")"
 
-# Install the Azure Artifacts Credential Provider
-if [ "${USENET6}" = "true" ]; then
-   export USE_NET6_ARTIFACTS_CREDENTIAL_PROVIDER=true
-fi
-wget -qO- https://aka.ms/install-artifacts-credprovider.sh | bash
-unset USE_NET6_ARTIFACTS_CREDENTIAL_PROVIDER
-
-sed "s/REPLACE_WITH_AZURE_DEVOPS_NUGET_FEED_URL_PREFIX/${PREFIXES}/g" ./scripts/run-dotnet.sh > /usr/local/bin/run-dotnet.sh
+sed "s|REPLACE_WITH_AZURE_DEVOPS_NUGET_FEED_URL_PREFIX|${PREFIXES}|g" ./run-dotnet.sh > /usr/local/bin/run-dotnet.sh
 chmod +rx /usr/local/bin/run-dotnet.sh
-sed "s/REPLACE_WITH_AZURE_DEVOPS_NUGET_FEED_URL_PREFIX/${PREFIXES}/g" ./scripts/run-nuget.sh > /usr/local/bin/run-nuget.sh
+sed "s|REPLACE_WITH_AZURE_DEVOPS_NUGET_FEED_URL_PREFIX|${PREFIXES}|g" ./run-nuget.sh > /usr/local/bin/run-nuget.sh
 chmod +rx /usr/local/bin/run-nuget.sh
+
+
+if command -v sudo >/dev/null 2>&1; then
+    if [ "root" != "$_REMOTE_USER" ]; then
+        if [ "${ALIAS_DOTNET}" = "true" ]; then
+            sudo -u ${_REMOTE_USER} bash -c "echo 'alias dotnet=/usr/local/bin/run-dotnet.sh'" >> ~/.bashrc
+            sudo -u ${_REMOTE_USER} bash -c "echo 'alias dotnet=/usr/local/bin/run-dotnet.sh'" >> ~/.zshrc
+        fi
+        if [ "${ALIAS_NUGET}" = "true" ]; then
+            sudo -u ${_REMOTE_USER} bash -c "echo 'alias nuget=/usr/local/bin/run-nuget.sh'" >> ~/.bashrc
+            sudo -u ${_REMOTE_USER} bash -c "echo 'alias nuget=/usr/local/bin/run-nuget.sh'" >> ~/.zshrc
+        fi
+        if [ "${USENET6}" = "true" ]; then
+            sudo -u ${_REMOTE_USER} bash -c "echo 'export USE_NET6_ARTIFACTS_CREDENTIAL_PROVIDER=true'" >> ~/.bashrc"
+            sudo -u ${_REMOTE_USER} bash -c "echo 'export USE_NET6_ARTIFACTS_CREDENTIAL_PROVIDER=true'" >> ~/.zshrc"
+        fi
+        exit 0
+    fi
+fi
 
 if [ "${ALIAS_DOTNET}" = "true" ]; then
     echo "alias dotnet='/usr/local/bin/run-dotnet.sh'" >> /etc/bash.bashrc
@@ -35,6 +47,11 @@ fi
 if [ "${ALIAS_NUGET}" = "true" ]; then
     echo "alias nuget='/usr/local/bin/run-nuget.sh'" >> /etc/bash.bashrc
     echo "alias nuget='/usr/local/bin/run-nuget.sh'" >> /etc/zsh/zshrc
+fi
+
+if [ "${USENET6}" = "true" ]; then
+   echo "export USE_NET6_ARTIFACTS_CREDENTIAL_PROVIDER=true" >> /etc/bash.bashrc
+   echo "export USE_NET6_ARTIFACTS_CREDENTIAL_PROVIDER=true" >> /etc/zsh/zshrc
 fi
 
 exit 0
--- src/artifacts-helper/run-nuget-sh
+++ src/artifacts-helper/run-nuget-sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Install artifact credential provider if it is not already installed
+if [ ! -d "${HOME}/.nuget/plugins/netcore" ]; then
+  wget -qO- https://aka.ms/install-artifacts-credprovider.sh | bash
+fi
+
 if [ -f "${HOME}/ado-auth-helper" ]; then
   export VSS_NUGET_ACCESSTOKEN=$(${HOME}/ado-auth-helper get-access-token)
   export VSS_NUGET_URI_PREFIXES=REPLACE_WITH_AZURE_DEVOPS_NUGET_FEED_URL_PREFIX
