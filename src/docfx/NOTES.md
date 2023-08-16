cf373ca2adf0f59c926fdc776e1677faff67df7f
Mark Phippard <markphip@gmail.com>
2023-08-16 11:17:19 -0400

    Update docfx to install a specific version

--- src/docfx/NOTES.md
+++ src/docfx/NOTES.md
@@ -1,6 +1,6 @@
-This feature currently only supports installing the latest published version
-of DocFX. If support for older versions are required please file an Issue or
-send a pull request.
+This feature installs the latest version compatible with Microsoft's internal documentation
+platform which is currently 2.67.5. You can install a different version or 'latest" by using
+the 'version' option.
 
 ## OS Support
 
--- test/_global/mariner_tests.sh
+++ test/_global/mariner_tests.sh
@@ -11,6 +11,7 @@ check "no-dirs"  grep -v "drwxr" <(ls -l /tmp/mariner_tests)
 check "git-config" grep "ado-auth-helper" <(cat /tmp/mariner_tests/.git/config)
 check "dotnet" grep "pkgs.dev.azure.com" <(cat /usr/local/bin/run-dotnet.sh)
 check "nuget" grep "pkgs.dev.azure.com" <(cat /usr/local/bin/run-nuget.sh)
+check "docfx-version" bash -c "docfx --version"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
