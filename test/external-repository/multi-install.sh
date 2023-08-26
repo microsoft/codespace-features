ac0f707475d56d28dff003f4dcdff998ded14777
Mark Phippard <markphip@gmail.com>
2023-08-26 10:08:04 -0400

    Fix bug when multi-repos clone has repo with . in name
    Added more tests to cover this scenario

--- test/external-repository/multi-install.sh
+++ test/external-repository/multi-install.sh
@@ -8,7 +8,7 @@ source dev-container-features-test-lib
 # Definition specific tests
 check "git-config-1" grep "ado-auth-helper" <(cat /tmp/multi-repos/community/.git/config)
 check "git-config-2" grep "ado-auth-helper" <(cat /tmp/multi-repos/spec/.git/config)
-check "git-config-3" grep "ado-auth-helper" <(cat /tmp/multi-repos/action/.git/config)
+check "git-config-3" grep "ado-auth-helper" <(cat /tmp/multi-repos/devcontainers.github.io/.git/config)
 
 # Report result
 reportResults
