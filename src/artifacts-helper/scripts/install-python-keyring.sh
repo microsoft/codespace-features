db89ea912056058b5240a0be6cf57b5024f12394
Mark Phippard <markphip@gmail.com>
2024-05-14 12:49:35 -0400

    Update install-python-keyring.sh

--- src/artifacts-helper/scripts/install-python-keyring.sh
+++ src/artifacts-helper/scripts/install-python-keyring.sh
@@ -3,13 +3,13 @@
 
 cd /tmp
 wget https://github.com/microsoft/codespace-features/releases/download/latest/codespaces_artifacts_helper_keyring-0.1.0-py3-none-any.whl
-if command -v python3 &> /dev/null
+if command -v pip3 &> /dev/null
 then
-    python3 -m pip install codespaces_artifacts_helper_keyring-0.1.0-py3-none-any.whl
-elif command -v python &> /dev/null
+    pip3 install codespaces_artifacts_helper_keyring-0.1.0-py3-none-any.whl
+elif command -v pip &> /dev/null
 then
-    python -m pip install codespaces_artifacts_helper_keyring-0.1.0-py3-none-any.whl
+    pip install codespaces_artifacts_helper_keyring-0.1.0-py3-none-any.whl
 else
-    echo "Python installation not detected. Artifacts Helper keyring not installed."
+    echo "pip installation not detected. Artifacts Helper keyring not installed."
     exit 1
 fi
