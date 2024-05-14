74f875cfb3f8ca2b467460805d7223c2317ad97b
Mark Phippard <markphip@gmail.com>
2024-05-14 16:44:01 +0000

    Improve Python/pip detection

--- src/artifacts-helper/scripts/install-python-keyring.sh
+++ src/artifacts-helper/scripts/install-python-keyring.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+
 cd /tmp
 wget https://github.com/microsoft/codespace-features/releases/download/latest/codespaces_artifacts_helper_keyring-0.1.0-py3-none-any.whl
-pip install codespaces_artifacts_helper_keyring-0.1.0-py3-none-any.whl
+if command -v python3 &> /dev/null
+then
+    python3 -m pip install codespaces_artifacts_helper_keyring-0.1.0-py3-none-any.whl
+elif command -v python &> /dev/null
+then
+    python -m pip install codespaces_artifacts_helper_keyring-0.1.0-py3-none-any.whl
+else
+    echo "Python installation not detected. Artifacts Helper keyring not installed."
+    exit 1
+fi
